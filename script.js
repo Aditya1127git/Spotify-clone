01596ed8d6278024df771132237dc63c58317638
Aditya <soubhagyalaxmis29@gmail.com>
2026-06-29 20:03:29 +0530

    updated js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ async function getSongs() {
     for (let index = 0; index < as.length; index++){
         const element = as[index];
         if(element.href.endsWith("mp3")){
-            songs.push(element.getAttribute("href"))
+            songs.push(element.href.split("/songs/")[1])
         }
     }
     return songs
@@ -20,10 +20,23 @@ async function getSongs() {
 async function main(){
     let songs = await getSongs()
     console.log("FIRST SONG =", songs[0])
+    console.log(songs);
+    console.log(songs.length);
+
+    let songUL = document.querySelector(".songList").getElementsByTagName("ul")[0]
+    for (const song of songs) {
+        songUL.innerHTML = songUL.innerHTML + `<li> ${song} </li>`;
+    }
 
     var audio = new Audio(songs[1]);
     audio.play();
     audio.pause();
+
+    audio.addEventListener("loadeddata", () => {
+        let duration = audio.duration;
+        console.log(duration)
+        //The duration variable now holds the duration (in seconds) of the audio clip
+    });
 }
 
 main()
